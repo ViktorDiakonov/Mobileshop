c015ae6cba99653ef7af321294f3c90ff2e60e55
ViktorDiakonov <vip3352@gmail.com>
2025-02-26 20:10:19 +0200

    -added page "error-404.html"

---
+++ src/main/java/ua/viktor/myspringbootapp/controllers/CustomErrorController.java
@@ -0,0 +1,27 @@
+package ua.viktor.myspringbootapp.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404";
+            }
+        }
+        return "error/404";
+    }
+}
+
---
+++ src/main/java/ua/viktor/myspringbootapp/util/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package ua.viktor.myspringbootapp.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/phones/**")
+                .addResourceLocations("file:uploads/phones/");
+    }
+}

537ff263e4e2a6ff906d830ab1e3022d14035522
ViktorDiakonov <vip3352@gmail.com>
2025-03-25 18:07:56 +0200

    added individual HTML file for users pages headers. Started create user cart.

---
+++ src/main/java/ua/viktor/myspringbootapp/controllers/CartController.java
@@ -0,0 +1,47 @@
+package ua.viktor.myspringbootapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ua.viktor.myspringbootapp.models.Cart;
+import ua.viktor.myspringbootapp.models.Phone;
+import ua.viktor.myspringbootapp.services.PhoneService;
+
+/**
+ * @author Diakonov Viktor
+ */
+@Controller
+@SessionAttributes("cart") // Корзина хранится в сессии
+public class CartController {
+    @ModelAttribute("cart") // Инициализация корзины
+    public Cart cart() {
+        return new Cart();
+    }
+        private final PhoneService phoneService;
+
+        @Autowired
+        public CartController(PhoneService phoneService) {
+            this.phoneService = phoneService;
+        }
+
+        @PostMapping("/cart/add/{phoneId}")
+        public String addToCart(
+                @PathVariable int phoneId,
+                @ModelAttribute("cart") Cart cart,
+                RedirectAttributes redirectAttributes
+        ) {
+            Phone phone = phoneService.findPhoneById(phoneId); // Получаем реальный телефон
+            cart.addItem(phone);
+            redirectAttributes.addFlashAttribute("message", "Товар добавлен в корзину!");
+            return "redirect:/mobileshop/phone/" + phoneId;
+        }
+
+    @GetMapping("/cart")
+    public String viewCart(@ModelAttribute("cart") Cart cart, Model model) {
+        model.addAttribute("items", cart.getItems());
+        model.addAttribute("total", cart.getTotal());
+        return "person/cart"; // Шаблон cart.html
+    }
+}
---
+++ src/main/java/ua/viktor/myspringbootapp/models/Cart.java
@@ -0,0 +1,23 @@
+package ua.viktor.myspringbootapp.models;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Diakonov Viktor
+ */
+@Getter
+public class Cart {
+    // Геттеры
+    private final List<Phone> items = new ArrayList<>();
+
+    public void addItem(Phone phone) {
+        items.add(phone);
+    }
+
+    public double getTotal() {
+        return items.stream().mapToDouble(Phone::getPrice).sum();
+    }
+}
